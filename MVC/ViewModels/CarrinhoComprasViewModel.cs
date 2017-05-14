51217f77d73ac7097787c79662bd1709cb54dfa8
Felipe Muniz <felipemunizicm2012@hotmail.com>
2017-05-14 19:15:22 -0300

    Exemplo pratico de ViewModel.

--- MVC/ViewModels/CarrinhoComprasViewModel.cs
+++ MVC/ViewModels/CarrinhoComprasViewModel.cs
@@ -3,8 +3,10 @@ using MVC.Models;
 
 namespace MVC.ViewModels
 {
-    public class CarrinhoCompras
+    public class CarrinhoComprasViewModel
     {
         public List<Produto> Produtos { get; set; }
+        public decimal ValorTotal { get; set; }
+        public string Menssagem { get; set; }
     }
 }
